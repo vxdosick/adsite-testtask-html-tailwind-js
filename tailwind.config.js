1b36cffc5ffe333185ebd1027020a96e00db56ba
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-09-03 18:33:31 +0200

    feat: Add header, footer, a bit of main and decor images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./dist/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue": "#0147FF",
+        "text": "#282828",
+      },
+      maxWidth: {
+        "wrapper": "1470px",
+        "footer": "100%"
+      },
+      width: {
+        "footer": "100%"
+      },
+      padding: {
+        "wrapper--px": "15px",
+      },
+      margin: {
+      }
+    },
   },
   plugins: [],
 }
