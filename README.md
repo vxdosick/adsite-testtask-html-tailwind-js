81d7207e7ff7408e9ff106058aca5922a78c9e05
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-09-07 22:13:14 +0200

    Add README file

---
+++ README.md
@@ -0,0 +1,21 @@
+# Car Information Landing Page
+
+This project is a landing page that provides information about cars. It uses HTML, Tailwind CSS, and JavaScript to create a modern, responsive design.
+
+## Installation
+1. Clone or download the project.
+2. Install dependencies by running:
+   ```bash
+   npm install
+   ```
+3. Start with live server.
+
+## Development
+To start the project in development mode, run:
+```bash
+npm run watch
+```
+
+## Development
+You can view the live project by visiting the following link: [https://vxdosick.github.io/adsite-testtask-html-tailwind-js/]
+
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,39 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 2,
+  spaceBetween: 64,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    850: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 64,
+    },
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 64,
+    }
+  }
+});
+
 const footerLastSect = document.querySelectorAll(".footer__opensect");
 
 footerLastSect.forEach(section => {
